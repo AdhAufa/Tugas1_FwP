879e9bac726aa201743e0461cbba9676da05dd66
Aufa Alaina Adhar <adhaufa@gmail.com>
2021-04-20 20:58:44 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container d-flex justify-content-center align-items-center" style = "height: 100vh">
+<div class="card rounded" style="width: 18rem;">
+  <img src="https://cdn.vox-cdn.com/thumbor/1mF1osW8F_F45P-OGznlrhgql2o=/0x0:1920x1080/920x613/filters:focal(788x238:1094x544):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/68758208/paimon.0.jpeg" class="card-img-top" alt="...">
+  <div class="card">
+  <div class="card-body">
+    <p class="card-text fw-bold text-center">TUGAS 1</p>
+  </div>
+</div>
+  <div class="card-body">
+    <p class="card-text">NIM : 16090154</p>
+    <p class="card-text">Nama : Aufa Alaina Adhar</p>
+    <p class="card-text">Kelas : 6A</p>
+  </div>
+</div>
+</div>
+</body>
+</html>
